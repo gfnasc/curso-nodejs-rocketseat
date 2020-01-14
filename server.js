76e6374ccae7fc27601f5535d022b35dfcdb8fa7
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2020-01-14 00:15:43 -0200

    criando conexao com o bando de dados e criando primeiro model

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 const app = express();
 
+mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true });
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
 app.get("/", (req, res) => {
-    res.send("Hello gui");
+    Product.create({
+        title: "Curso Nodejs",
+        description: "Projeto do curso nodejs rocketseat",
+        url: "http://gfnasc.com/"
+    });
+    return res.send("Hello gui");
 });
 
 app.listen(3001);
