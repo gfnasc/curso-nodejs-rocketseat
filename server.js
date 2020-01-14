1e249209cbacde38996e10c4f697657ad9672fcd
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2020-01-14 00:32:41 -0200

    reestruturando o projeto

--- server.js
+++ server.js
@@ -8,15 +8,6 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", { useNewUrlParser: true })
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
-
-app.get("/", (req, res) => {
-    Product.create({
-        title: "Curso Nodejs",
-        description: "Projeto do curso nodejs rocketseat",
-        url: "http://gfnasc.com/"
-    });
-    return res.send("Hello gui");
-});
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
